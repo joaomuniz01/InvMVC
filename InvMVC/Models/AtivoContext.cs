b589be3aa488c5b7675f65be3da73b1658b8304c
João Muniz <joao.muniz01@fatec.sp.gov.br>
2016-12-09 01:47:19 -0200

    Context, Migrations, db added

---
+++ InvMVC/Models/AtivoContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace InvMVC.Models
+{
+    public class AtivoContext : DbContext
+    {
+        public AtivoContext() : base("AtivoContext") { }
+
+        public DbSet<Ativo> Ativos { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
