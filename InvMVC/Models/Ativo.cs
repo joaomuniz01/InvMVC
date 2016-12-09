a5a348fa1ba482aa3bdd4e1832b47fb2757b47f6
João Muniz <joao.muniz01@fatec.sp.gov.br>
2016-12-09 01:28:43 -0200

    Models criados, EF e Bootstrap custom

---
+++ InvMVC/Models/Ativo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InvMVC.Models
+{
+    public class Ativo
+    {
+        public int AtivoId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Descricao { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Tipo { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Local { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+    }
+}
---
+++ InvMVC/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InvMVC.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        public virtual List<Ativo> Ativos { get; set; }
+    }
+}
